1755b7f2aa3a543973122370caa353fdc8fc238c
harshil <harshilpatel2391@gmail.com>
2022-08-29 15:03:33 -0400

    timer functinality completed

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start);
-    }
-
-    start() {
-        console.log("time to start the timer");
-    }
-}
-
 const durationInput = document.body.querySelector("#duration");
 const startButton = document.body.querySelector("#start");
 const pauseButton = document.body.querySelector("#pause");
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+
+    },
+    onTick() {
+
+    },
+    onComplete() {
+
+    }
+});
