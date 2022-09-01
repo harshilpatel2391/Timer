6294de697a35010262f8a4e9c3ee116fe8237caf
harshil <harshilpatel2391@gmail.com>
2022-09-01 14:38:17 -0400

    Timer project completed

--- Timer.js
+++ Timer.js
@@ -15,10 +15,10 @@ class Timer {
 
     start = () => {
         if(this.onStart) {
-            this.onStart();
+            this.onStart(this.timeRemaining);
         }
         this.tick();
-        this.intervalId = setInterval(this.tick, 1000);
+        this.intervalId = setInterval(this.tick, 100);
     }
 
     tick = () => {
@@ -28,9 +28,9 @@ class Timer {
                 this.onComplete();
             }
         } else {
-            this.timeRemaining = this.timeRemaining - 1;
+            this.timeRemaining = this.timeRemaining - 0.1;
             if(this.onTick) {
-                this.onTick();
+                this.onTick(this.timeRemaining);
             }
         }
     }
@@ -44,6 +44,6 @@ class Timer {
     }
 
     set timeRemaining(time) {
-        this.durationInput.value = time;
+        this.durationInput.value = time.toFixed(2);
     }
 }
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const durationInput = document.body.querySelector("#duration");
 const startButton = document.body.querySelector("#start");
 const pauseButton = document.body.querySelector("#pause");
+
+const circle = document.body.querySelector('circle');
+const perimeter = 2 * Math.PI * circle.getAttribute('r');
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
+    onStart(totalDuration) {
+        duration = totalDuration;
+        circle.setAttribute('stroke-dasharray', perimeter);
 
     },
-    onTick() {
-
+    onTick(timeRemaining) {
+        circle.setAttribute(
+            'stroke-dashoffset',
+            (perimeter * timeRemaining) / duration - perimeter
+          );
     },
     onComplete() {
 
